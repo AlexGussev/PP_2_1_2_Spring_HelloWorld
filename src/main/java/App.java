fab1d70315858aa2628d7ca5b5c02cd91f2245e0
AlexKing <pikashev171@mail.ru>
2022-08-07 23:06:22 +0500

    My commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBin =
+                (Cat) applicationContext.getBean("cat");
+        Cat catBin1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.equals(bean1) + " " + catBin.equals(catBin1));
     }
 }
